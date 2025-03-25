d685e8775ea5991d4307e545d828bb347e50920f
projetowordgame <projetowordgame@gmail.com>
2025-03-25 07:12:59 -0300

    Correção de digitação do import header.css
    Tentei realizar o commit usando uma feature mas não tive permição, precisei corrigir a digitação do importe, pois estava impactando na execução do programa.
    
    Thiago

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
-import "./Header.css"; // Importa o CSS
+import "./header.css"; // Importa o CSS
 
 const Header = () => {
   const [user, setUser] = useState(null);
