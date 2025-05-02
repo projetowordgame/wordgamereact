d9476c714195d5dc3bdcb491edee45d6c6be524e
Thiago Silva de Oliveira <thiagodevoliveira@hotmail.com>
2025-05-02 18:14:49 -0300

    Ajustes da tela Home

--- src/Home.jsx
+++ src/Home.jsx
@@ -11,27 +11,20 @@ const Home = () => {
   return (
         
     <div className="home-container">
-      <Header user={user} />
-      <main>
-        <h1><span id="montain">&#128507;</span>Agora você pode aprender se divertindo!<span>&#127919;</span></h1>        
+      <Header user={user}/>
+      <main class="container-logo">
+        <img src="/images/logoKidsGames.jpeg" alt="Logo"/>
+        <h1 className="head">Agora ensinar e aprender ficou mais divertido!<span>&#127919;</span></h1>        
       </main>
-
-      <div className="head">
-        <h2 className="head">Aprender virou uma grande diversão!</h2>
-      </div>
-
-      <section>
-                
+      <section>                
         <div>
-          <h3>O que é o Learn by Playing?</h3>
-          <p>O Learn by Playing é uma plataforma de aprendizado interativa que transforma o ensino e aprendizado em uma experiência divertida e envolvente. Com jogos educativos, quizzes e atividades dinâmicas, os alunos podem aprender enquanto se divertem.</p>  
+          <h3>O que é o Kids Games?</h3>
+          <p>O Kids Games é uma plataforma de aprendizado interativa que transforma o ensino e aprendizado em uma experiência divertida e envolvente. Com jogos educativos, quizzes e atividades dinâmicas, os alunos podem aprender enquanto se divertem.</p>  
         </div>
-
         <div>
           <h3>Como funciona?</h3>
           <p>Os professores podem criar atividades personalizadas ativando assim a criatividade e engajamento dos alunos, despertando o interesse dos alunos que têm uma experiência de aprendizado mais dinâmica e divertida, tendo acesso a uma variedade de jogos e quizzes para praticar o que aprenderam enquanto o professor avalia o nível de aprendizado e interesse dos alunos. </p>
         </div>
-
         <div> 
           <h3>Recursos principais</h3>
           <ul>
@@ -42,28 +35,22 @@ const Home = () => {
             <li>Atividades personalizadas</li>
           </ul>
         </div>
-
         <div>
-          <h3>Benefícios do Learn by Playing</h3>
-          <p>Com o Learn by Playing, os alunos podem aprender de forma mais eficaz e interativa. A plataforma é projetada para ajudar os alunos a reter informações de maneira mais eficiente, tornando o aprendizado uma experiência agradável.</p>
+          <h3>Benefícios do Kids Games</h3>
+          <p>Com o Kids Games, os alunos podem aprender de forma mais eficaz e interativa. A plataforma é projetada para ajudar os alunos a reter informações de maneira mais eficiente, tornando o aprendizado uma experiência agradável.</p>
         </div>
-
         <div>
           <h3>Quem pode usar?</h3>
-          <p>O Learn by Playing é ideal para alunos de todas as idades, professores e educadores. A plataforma é projetada para ser acessível e fácil de usar, permitindo que todos aproveitem os benefícios do aprendizado baseado em jogos.</p> 
+          <p>O Kids Games é ideal para alunos de todas as idades, professores e educadores. A plataforma é projetada para ser acessível e fácil de usar, permitindo que todos aproveitem os benefícios do aprendizado baseado em jogos.</p> 
         </div>
-
         <div>
           <h3>Como começar?</h3>
-          <p>Para começar a usar o Learn by Playing, basta se inscrever na nossa plataforma. Você pode criar uma conta gratuita e começar a explorar nossos jogos e atividades educativas. É fácil e rápido!</p>  
+          <p>Para começar a usar o Kids Games, basta se inscrever na nossa plataforma. Você pode criar uma conta gratuita e começar a explorar nossos jogos e atividades educativas. É fácil e rápido!</p>  
         </div>
-
-      </section>
-      
+      </section>      
       <footer>
-        2025 Learn by Playing. Todos os direitos reservados.
+        2025 Kids Games. Todos os direitos reservados.
       </footer>
-
     </div>
   );
 };
